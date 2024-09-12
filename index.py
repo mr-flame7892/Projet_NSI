8d9179dffff9d308d762e73a5d82f0571b47f79a
MR.Flame <67482496+mr-flame7892@users.noreply.github.com>
2024-09-12 18:51:56 +0200

    Mise à jour de l'index.py
    Ajout du système de rareté avec les coffres

--- index.py
+++ index.py
@@ -1,1 +1,30 @@
+import random
 
+def rarityPicker():
+    chance = random.randint(1, 100)
+    if 35 <= chance <= 100:
+        return "commun"
+    elif 10 <= chance < 35:
+        return "rare"
+    elif 5 < chance < 10:
+        return "épique"
+    elif 1 < chance < 5:
+        return "légendaire"
+    elif chance == 1:
+        return "mythique"
+
+class chests:
+    def __init__(self):
+        self.length = 1000000000
+        self.currentRarity = rarityPicker()
+    def openChest(self):
+        if self.length == 0:
+            return "Vous n'avez pas de coffre !"
+        self.length = self.length - 1
+        print(f"Vous avez obtenu un item {self.currentRarity} !\nIl vous reste {self.length} coffre(s) !")
+        self.currentRarity = rarityPicker()
+        return
+        
+
+        
+chest = chests()
