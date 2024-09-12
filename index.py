fa3f94f105672682ba6eba6972014c839fa88e2c
MatouLaRacaille <dlt.grimm@gmail.com>
2024-09-12 19:48:00 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,32 @@
 import random
 
+
+    
+class character:
+    def __init__(self):
+        self.pv = 100
+        self.attack = 5
+        self.defence = 5
+        self.items = []
+        self.exp = 0
+        self.limitExp = 1
+        self.lvl = 1
+        self.chests = 0
+
+character = character()
+
+
+class monstres:
+    def __init__(self):
+        self.pv = 95
+        self.attack = 4
+        self.defence = 4
+        self.boss = random.randint(0,100)
+
+monstre = monstres()
+
+
+
 def rarityPicker():
     chance = random.randint(1, 100)
     if 35 <= chance <= 100:
@@ -12,26 +39,6 @@ def rarityPicker():
         return "légendaire"
     elif chance == 1:
         return "mythique"
-    
-class character:
-    def __init__(self):
-        self.pv = 100
-        self.attack = 5
-        self.defence = 5
-        self.items = []
-        self.exp = 0
-        self.limitExp = 1
-        self.lvl = 1
-        self.chests = 1
-
-character = character()
-
-
-class monstres:
-    def __init__(self):
-        self.pv = 95
-        self.attack = 4
-        self.defence = 4
        
 
 class chestsSystem:
