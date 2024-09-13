bcb5252e619ad880cf0074a26e20841b249aabcd
MR.Flame <67482496+mr-flame7892@users.noreply.github.com>
2024-09-13 22:29:11 +0200

    Update index.py
    Ajout du premier combat + ajout de méthodes pour le personnage + création des fonctions pour faire attaquer le monstre et le personnage + calcul des dégâts

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import random
 
+game = True
+
 class character:
     def __init__(self):
         self.startPV = 100
@@ -7,9 +9,9 @@ class character:
         self.startDef = 5
         self.startSpeed = 30
         self.items = { "PotionSoin": 2, "PotionMana": 2}
-        self.armor = { "name": "Tunique de Cuire", "pv": 10/100, "def": 25/100}
-        self.boots = { "name": "Bottes de cuir", "speed": 15/100 }
-        self.weapon = { "name": "Branche d'Arbre", "dmg": 5/100 }
+        self.armor = { "name": "Tunique de Cuire", "pv": 15/100, "def": 25/100}
+        self.boots = { "name": "Bottes de cuir", "speed": 10/100 }
+        self.weapon = { "name": "Branche d'Arbre", "dmg": 20/100 }
         self.exp = 0
         self.limitExp = 1
         self.lvl = 1
@@ -25,15 +27,33 @@ class character:
     def seeInventory(self):
         print(f"Votre personnage à :\n\n- Armure : {self.armor['name']}\n- Bottes : {self.boots['name']}\n- Arme : {self.weapon['name']}")
         return
+    def calculateStats(self):
+        self.currentPV = self.startPV + self.startPV * self.armor["pv"]
+        self.currentDef = self.startDef + self.startDef * self.armor["def"]
+        self.currentSpeed = self.startSpeed + self.startSpeed * self.boots["speed"]
+        self.currentDMG = self.startDMG + self.startDMG * self.weapon["dmg"]
+        return
+    def resetCharacter(self):
+        self.items = { "PotionSoin": 2, "PotionMana": 2}
+        self.armor = { "name": "Tunique de Cuire", "pv": 15/100, "def": 25/100}
+        self.boots = { "name": "Bottes de cuir", "speed": 10/100 }
+        self.weapon = { "name": "Branche d'Arbre", "dmg": 20/100 }
+        self.exp = 0
+        self.limitExp = 1
+        self.lvl = 1
+        self.chests = 0
+        self.room = 1
+        self.calculateStats()
+            
 
 character = character()
-
 
 class monstres:
     def __init__(self):
         self.pv = 95
         self.attack = 4
         self.defence = 4
+        self.speed = 25
         self.boss = False
 
         chanceBoss = random.randint(1,100)
@@ -71,4 +91,45 @@ class chestsSystem:
         
 chest = chestsSystem()
 
-chest.openChest()
+DMG = None
+randomDMG = None
+
+def characterAttacks():
+        randomDMG = random.randint(1, 10)
+        DMG = round((randomDMG * character.currentDMG) - ((randomDMG * character.currentDMG) * monstre.defence/100), 0)
+        monstre.pv = round(monstre.pv - DMG, 0)
+        if monstre.pv <= 0:
+            print(f"--------------------------------\nVous avez triomphé du mal, cependant il vous reste du chemin à parcourir...")
+        else:
+            print(f"--------------------------------\nLe monstre s'est pris {(random.randint(1, 5) * character.currentDMG) - ((random.randint(1, 5) * character.currentDMG) * monstre.defence/100)} DMG\nIl lui reste {monstre.pv} PV !")
+            input("--------------------------------\nAppuyez sur Entrée pour continuer le combat")
+            monstreAttacks()
+            
+def monstreAttacks():
+        randomDMG = random.randint(1, 5)
+        DMG = round((randomDMG * monstre.attack) - ((randomDMG * monstre.attack) * character.currentDef/100), 0)
+        character.currentPV = round(character.currentPV - DMG, 0)
+        if character.currentPV <= 0:
+            return print(f"--------------------------------\nLe mal a eu raison de vous...\nIl ne lui restait plus que {monstre.pv} PV--------------------------------\n")
+        else:
+            print(f"--------------------------------\nVous vous êtes pris {(random.randint(1, 5) * character.currentDMG) - ((random.randint(1, 5) * character.currentDMG) * monstre.defence/100)} DMG\nIl vous reste {character.currentPV} PV !")
+            input("--------------------------------\nAppuyez sur Entrée pour continuer le combat")
+            characterAttacks()
+
+def launchGame():
+    while game == True:
+        while monstre.pv >= 0 and character.currentPV >= 0:
+            if monstre.speed < character.currentSpeed:
+                characterAttacks()
+            else:
+                monstreAttacks()
+                  
+
+tuto = input("tuto ? (y/n)")
+
+if tuto == "y":
+    print("Chère joueur, vous aller découvrir un jeux codé grâce au connaissance acquise en spé N.S.I :\n-----------------------\nLe jeux se joue avec la console de Thonny et est uniquement textuel suivez les instructions et profiter du jeu\n----------------------- ")
+    input("Appuyez sur entrée quand vous êtes prêt !")
+    launchGame()
+else:
+    launchGame()
