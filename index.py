8820236710f27144470f4d58562580450c3b5b1f
MatouLaRacaille <dlt.grimm@gmail.com>
2024-09-18 16:19:25 +0200

    Update index.py

--- index.py
+++ index.py
@@ -100,33 +100,6 @@ class monstres:
 
 monstre = monstres()
 
-"""def rarityPicker():
-    chance = random.randint(1, 100)
-    if 35 <= chance <= 100:
-        return "commun"
-    elif 10 <= chance < 35:
-        return "rare"
-    elif 5 < chance < 10:
-        return "épique"
-    elif 1 < chance < 5:
-        return "légendaire"
-    elif chance == 1:
-        return "mythique"
-
-class chestsSystem:
-    def __init__(self):
-        self.currentRarity = rarityPicker()
-        self.item = "Bâton"
-    def openChest(self):
-        if character.chests == 0:
-            return "Vous n'avez pas de coffre !"
-        character.chests = character.chests - 1
-        character.items.append(self.item)
-        print(f"Vous avez obtenu un item {self.currentRarity} !\nIl vous reste {character.chests} coffre(s) !")
-        self.currentRarity = rarityPicker()
-        return
-        
-chest = chestsSystem()"""
 
 def shop():
     vente=[item[random.randint(0,len(item)-1)],item[random.randint(0,len(item)-1)],item[random.randint(0,len(item)-1)]]
